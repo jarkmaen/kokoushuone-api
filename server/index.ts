dfc7020375eedae88b1e3c5945dc84ea5f13e775
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-01-28 13:49:43 +0200

    lisäys: palvelimen käynnistys virheenkäsittely

--- server/index.ts
+++ server/index.ts
@@ -2,4 +2,15 @@ import * as logger from "./utils/logger.js";
 import app from "./app.js";
 import { PORT } from "./utils/config.js";
 
-app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+const start = () => {
+    const server = app.listen(PORT, () => {
+        logger.info(`Server running on port ${PORT}`);
+    });
+
+    server.on("error", (error) => {
+        logger.error("Failed to start the server:", error);
+        process.exit(1);
+    });
+};
+
+start();

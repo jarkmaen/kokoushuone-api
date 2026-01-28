26d01d3f67ae32a863114e425abd9a872ae1471a
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-01-28 14:01:23 +0200

    lisäys: cors kirjasto asennettu ja otettu käyttöön

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,12 @@
-import * as logger from "./utils/logger.js";
+import cors from "cors";
 import express from "express";
+
+import * as logger from "./utils/logger.js";
 import reservationsRouter from "./routes/reservations.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/", reservationsRouter);
